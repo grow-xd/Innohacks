6a75ecbeccb3d31274c95e2053fa0b0fac7dc72d
Param sharma <kaushik.param2004@gmail.com>
2024-03-06 10:14:29 +0530

    ui init

---
+++ Frontend/frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './index.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className='text-3xl text-center m-2 rounded-xl bg-blue-200 shadow-xl hover:bg-blue-500'>Hello !!!</h1>
+    </>
+  )
+}
+
+export default App
